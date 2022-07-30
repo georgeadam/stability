76393f9f9fbb3f5755f0bb9d49c4fea92c6aa0f2
alexadam <paddze21@gmail.com>
2022-07-30 17:24:36 -0400

    Added LeNet from Churn Reduction via Distillation

---
+++ src/models/creation.py
@@ -0,0 +1,3 @@
+from src.base.factory import Factory
+
+models = Factory()
---
+++ src/models/lenet.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+
+from .creation import models
+
+
+class LeNet(nn.Module):
+    def __init__(self, output_dim, size):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16 * size, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=16 * size, out_channels=24 * size, kernel_size=5)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(in_features=384 * size, out_features=128 * size)
+        self.fc2 = nn.Linear(in_features=128 * size, out_features=84)
+        self.fc3 = nn.Linear(84, output_dim)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool2(x)
+
+        x = x.view(x.shape[0], -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+
+        return self.fc3(x)
+
+
+models.register_builder("lenet", LeNet)
