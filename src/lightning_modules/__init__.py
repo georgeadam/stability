1f4b2b61e0a732478fc9d0a4d91e27f6b90f54c0
alexadam <paddze21@gmail.com>
2022-08-18 16:24:25 -0400

    Added sequential trainer

--- src/lightning_modules/__init__.py
+++ src/lightning_modules/__init__.py
@@ -3,4 +3,5 @@ from .distill import Distill
 from .improved_kd import ImprovedKD
 from .pc_training import PCTraining
 from .reweight import Reweight
+from .sequential import Sequential
 from .standard import Standard
---
+++ src/lightning_modules/sequential.py
@@ -0,0 +1,67 @@
+import torch
+import wandb
+from pytorch_lightning import LightningModule
+from torchmetrics.functional import accuracy
+
+from .creation import lightning_modules
+
+
+class Sequential(LightningModule):
+    def __init__(self, model, original_model, lr):
+        super().__init__()
+
+        self.model = model
+        self.original_model = original_model
+        self.lr = lr
+        self.loss = torch.nn.CrossEntropyLoss(reduction='none')
+
+    def forward(self, batch):
+        # Used only by trainer.predict() to evaluate the model's predictions
+        x, y = batch
+
+        logits = self.model(x)
+
+        return logits
+
+    def training_step(self, batch, batch_idx):
+        _, loss, acc = self._get_preds_loss_accuracy(batch)
+
+        # Log loss and metric
+        self.log('train/loss', loss, on_step=False, on_epoch=True)
+        self.log('train/accuracy', acc, on_step=False, on_epoch=True)
+        self.log('train/epoch', self.current_epoch, on_step=False, on_epoch=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        if self.global_step == 0:
+            wandb.define_metric('val/accuracy', summary='max')
+
+        preds, loss, acc = self._get_preds_loss_accuracy(batch)
+
+        # Log loss and metric
+        self.log('val/loss', loss, on_step=False, on_epoch=True)
+        self.log('val/accuracy', acc, on_step=False, on_epoch=True)
+        self.log('val/epoch', self.current_epoch, on_step=False, on_epoch=True)
+
+        return preds
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.model.parameters(), lr=self.lr)
+
+    def _get_preds_loss_accuracy(self, batch):
+        x, y = batch
+        logits = self.model(x)
+        preds = torch.argmax(logits, dim=1)
+        loss = self.loss(logits, y)
+        acc = accuracy(preds, y)
+
+        original_preds = torch.argmax(self.original_model(x), dim=1)
+        original_correct = (original_preds == y)
+
+        loss = loss[original_correct].mean()
+
+        return preds, loss, acc
+
+
+lightning_modules.register_builder("sequential", Sequential)
