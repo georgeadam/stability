69e087345683e33ad3c4ca37e0256b78fe771f51
alexadam <paddze21@gmail.com>
2022-09-02 14:59:34 -0400

    Added learning based sample scorer

--- src/callbacks/sample_scorers/__init__.py
+++ src/callbacks/sample_scorers/__init__.py
@@ -1,4 +1,5 @@
 from .creation import scorers
 from .flip_scorer import FlipScorer
+from .learnt_scorer import LearntScorer
 from .self_taught import SelfTaughtScorer
 from .standard import StandardScorer
---
+++ src/callbacks/sample_scorers/learnt_scorer.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+
+from .creation import scorers
+from .scorer import Scorer
+
+
+class LearntScorer(Scorer):
+    def __init__(self):
+        self.predictions = pd.DataFrame({"preds": [], "y": [],
+                                         "correct": [], "epoch": [], "index": []})
+        self.predictions = self.predictions.astype(int)
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        outputs = pl_module.training_outputs
+
+        outputs = {k: outputs[k] for k in self.predictions.keys()}
+        outputs = pd.DataFrame(outputs)
+        self.predictions = pd.concat([self.predictions, outputs])
+
+    def generate_scores(self):
+        self.predictions["adjusted"] = 1 - self.predictions["correct"]
+        correct_to_end_indices = self.predictions[::-1].astype(int).groupby(["index"])["adjusted"].cumsum().eq(0)
+        learnt = self.predictions[correct_to_end_indices.values[::-1]].groupby(["index"])["epoch"].min()
+
+        # now we have the epoch at which a sample is first permanently learned, but this only works for samples that
+        # are permanently learned, so we have to add the rest manually with the max value
+
+        unique_indices = np.array(self.predictions["index"].unique())
+        not_learnt = np.setdiff1d(unique_indices, learnt.index)
+        max_epoch = self.predictions["epoch"].max()
+        not_learnt = pd.Series(np.ones(len(not_learnt)) * max_epoch, index=not_learnt)
+
+        combined = pd.concat([learnt, not_learnt])
+
+        return np.array(combined.index), combined.values
+
+
+scorers.register_builder("learnt", LearntScorer)
