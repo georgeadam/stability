38d893db6e72206ac28367512ee7e4a2e27fd15e
alexadam <paddze21@gmail.com>
2022-09-06 15:44:32 -0400

    Fixed another early stopping bug
    -Hopefully the last. This one was caused by using the on_validation_end
    hook instead of the on_train_epoch_end hook. Issue is that this former
    hook gets called prior to the start of training which means that
    the "best_score" will potentially be a value that's not even seen
    in the wandb graphs. This resulted in early stopping happening
    after 5 epochs for many of the runs.

--- src/callbacks/custom/curriculum_early_stopping.py
+++ src/callbacks/custom/curriculum_early_stopping.py
@@ -10,12 +10,12 @@ class CurriculumEarlyStopping(EarlyStopping):
         self.epochs_until_full = epochs_until_full
 
     def on_train_epoch_end(self, trainer, pl_module) -> None:
-        pass
+        if trainer.current_epoch >= self.epochs_until_full:
+            self._run_early_stopping_check(trainer)
 
     def on_validation_end(self, trainer, pl_module):
         # override this to disable early stopping at the end of val loop
-        if trainer.current_epoch >= self.epochs_until_full:
-            self._run_early_stopping_check(trainer)
+        pass
 
 
 custom_callbacks.register_builder("curriculum_early_stopping", CurriculumEarlyStopping)
