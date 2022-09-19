4e4795608f90565f5d78bd5a89bbf6b1c8d06fd8
alexadam <paddze21@gmail.com>
2022-09-18 20:48:25 -0400

    Added forward_embedding method to lenet

--- src/models/lenet.py
+++ src/models/lenet.py
@@ -40,5 +40,22 @@ class LeNet(nn.Module):
 
         return self.fc3(x)
 
+    def forward_embedding(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool2(x)
+
+        x = x.view(x.shape[0], -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+
+        return x
+
 
 models.register_builder("lenet", LeNet)
