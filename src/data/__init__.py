9ebbeb3be08222fd45d63961a4a0f67b66df0f4e
alexadam <paddze21@gmail.com>
2022-09-23 14:19:31 -0400

    Added EMNIST dataset

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,6 +2,7 @@ from .celeba import CelebADataModule
 from .cifar10 import CIFAR10DataModule
 from .cifar100 import CIFAR100DataModule
 from .creation import datasets
+from .emnist import EMNISTDataModule
 from .fashion_mnist import FashionMNISTDataModule
 from .mnist import MNISTDataModule
 from .svhn import SVHNDataModule
