d45ddc8c2160683b06b99f17391da93c427d973d
alexadam <paddze21@gmail.com>
2022-09-27 16:24:13 -0400

    Replaced MobileNetv3 with MobileNetv2

--- src/models/mobilenet.py
+++ src/models/mobilenet.py
@@ -1,9 +1,9 @@
-from torchvision.models import mobilenet_v3_small
+from torchvision.models import mobilenet_v2
 
 from .creation import models
 
 
 def mobilenet_wrapper(num_channels, height, **kwargs):
-    return mobilenet_v3_small(**kwargs)
+    return mobilenet_v2(**kwargs)
 
 models.register_builder("mobilenet", mobilenet_wrapper)
