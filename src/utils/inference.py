988a8735a6ec55a495d8dd30e101c9122c8a8b46
alexadam <paddze21@gmail.com>
2022-12-18 16:01:41 -0500

    Added extract_probabilities inference util

--- src/utils/inference.py
+++ src/utils/inference.py
@@ -13,3 +13,16 @@ def extract_predictions(dataloader, model):
     preds = torch.argmax(logits, dim=1)
 
     return preds.detach().cpu().numpy()
+
+
+def extract_probabilities(dataloader, model):
+    module = lightning_modules.create("prediction", model=model)
+    trainer = Trainer(gpus=1, enable_checkpointing=False, enable_progress_bar=False)
+
+    logits = trainer.predict(module, dataloaders=dataloader)
+    logits = torch.cat(logits)
+    preds = torch.argmax(logits, dim=1)
+    probs = torch.nn.Softmax(dim=1)(logits)
+    sample_indices = torch.arange(len(probs))
+
+    return probs[sample_indices, preds].detach().cpu().numpy()
